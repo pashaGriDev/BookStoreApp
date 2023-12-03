80af3c7ac59cc58095469954b03b8206184a60bf
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-12-03 16:00:36 +0400

    Initial Commit

---
+++ BookStoreApp/BookStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreAppApp.swift
+//  BookStoreApp
+//
+//  Created by Павел Грицков on 03.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookStoreApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookStoreApp
+//
+//  Created by Павел Грицков on 03.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
