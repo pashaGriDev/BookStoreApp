4f6e8189fab0b1a6f12e5a65f152e4d6dd6ff500
Alex Zhulai <Lepiozdyx@gmail.com>
2023-12-08 10:37:09 -0300

    Create DetailView + Custom buttons view

---
+++ BookStoreApp/ButtonView.swift
@@ -0,0 +1,29 @@
+//
+//  ButtonView.swift
+//  BookStoreApp
+//
+//  Created by Alex on 05.12.2023.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    let title: String
+    let background: Color
+    let foreground: Color
+    let action: () -> Void
+    
+    
+    var body: some View {
+        Button(title, action: action)
+            .font(.subheadline)
+            .frame(width: 161, height: 40)
+            .background(background)
+            .foregroundStyle(foreground)
+            .clipShape(.rect(cornerRadius: 5))
+    }
+}
+
+#Preview {
+    ButtonView(title: "Add to list", background: .secondary, foreground: .black, action: {})
+}
