e427a551eddb7f978aa0054ada415526ff3c1d73
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-12-13 22:34:56 +0300

    changed the categories

---
+++ BookStoreApp/Categories/CategoriesView.swift
@@ -0,0 +1,48 @@
+//
+//  CategoriesView.swift
+//  BookStoreApp
+//
+//  Created by Мирсаит Сабирзянов on 13.12.2023.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    
+    @StateObject var viewModel = CategoryViewModel()
+    
+    @State private var searchText = ""
+    @State private var sortIsActive = false
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    SearchBarView(searchText: $searchText, sortIsActive: $sortIsActive)
+                        .padding(.top)
+                        
+                    HStack {
+                        Text("Categories")
+                            .font(.system(size: 25, weight: .regular))
+                        Spacer()
+                    }
+                    .padding()
+                    CategoriesRowView(categories: viewModel.MockCategories, sort: sortIsActive)
+                }
+            }
+        }
+    }
+}
+
+
+
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
+
+#Preview {
+    CategoryView()
+}
---
+++ BookStoreApp/Categories/CategoryViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryViewModel.swift
+//  BookStoreApp
+//
+//  Created by Мирсаит Сабирзянов on 13.12.2023.
+//
+
+import Foundation
+
+class CategoryViewModel: ObservableObject {
+    
+    var service = Network<Endpoint>()
+    
+    var MockCategories = ["Non-fiction", "Classics",
+                      "Fantasy", "Young Adult",
+                      "Crime", "Horror",
+                      "Sci-fi", "Drama",
+                      "Folk"]
+    
+    func getData() {
+        Task {
+
+        }
+    }
+}
