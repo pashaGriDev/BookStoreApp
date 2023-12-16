2f402d2dec0a20afff672737852d9a891c928336
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-12-16 19:52:14 +0400

    update detail view, add url extension with 2 func

--- BookStoreApp/Helper/URL + ext.swift
+++ BookStoreApp/Helper/URL + ext.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension URL {
+    static func createUrlBy(id: Int) -> Self {
+        let url = URL(string: "https://covers.openlibrary.org/b/id/\(id)-M.jpg")!
+        return url
+    }
+    
+    static func createUrlBy(olid: String) -> Self {
+        let url = URL(string: "https://covers.openlibrary.org/b/olid/\(olid)-M.jpg")!
+        return url
+    }
+}
+
